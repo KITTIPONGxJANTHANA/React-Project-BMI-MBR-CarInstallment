dc0d9c7573f09bee7c76b08c7771944cc332438e
KITTIPONGxJANTHANA <s6752410027@sau.ac.th>
2025-10-27 23:38:26 +0700

    V.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./component/Home";
 import BMR from "./component/BMR";
-import BMI from "./component/BMI";
+import BMI from "./component/Bmi";
 import Car from "./component/Car";
 
 
